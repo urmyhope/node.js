ac2e9234025fb79549032e854e532db3e5de5380
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-04-23 08:58:01 -0300

    Create id.js

---
+++ id.js
@@ -0,0 +1,8 @@
+let idade = 15;
+if(idade >= 18){
+console.log("Sua idade é: " + idade + "Você é maior de idade");
+}
+else {
+    console.log("Sua idade é: " + idade + "Você é menor de idade");
+    
+}
