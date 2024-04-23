577629ff435eaa9fbcea81647dbbc63ddb91c89a
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-04-23 09:17:11 -0300

    Update fatorial.js

--- fatorial.js
+++ fatorial.js
@@ -1,5 +1,18 @@
 const fatorial = 5;
-let resultado =0;
+let resultado = 0;
 
 resultado = fatorial * (fatorial - 1)*(fatorial - 2)*(fatorial - 3)*(fatorial - 4);
 console.log("0 resultado é: " + resultado)
+
+
+const fatorial1 = 6;
+let resultadoq = 0;
+
+resultadoq = fatorial1 * (fatorial1 - 1)*(fatorial1 - 2)*(fatorial1 - 3)*(fatorial1 - 4)*(fatorial1 - 5);
+console.log("0 resultado é: " + resultadoq)
+
+const fatorial2 = 10;
+let resultadot = 0;
+
+resultadot = fatorial2 * (fatorial2 - 1)*(fatorial2 - 2)*(fatorial2 - 3)*(fatorial2 - 4)*(fatorial2 - 5)*(fatorial2 - 6)*(fatorial2 - 7)*(fatorial2 - 8)*(fatorial2 - 9);
+console.log("0 resultado é: " + resultadot)
