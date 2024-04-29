26880fcb650814f5a4d0880c49b6d2b5a9651d08
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-04-29 08:33:49 -0300

    Create cadastro.js

---
+++ cadastro.js
@@ -0,0 +1,27 @@
+print("Faça o cadastro da primeira pessoa aqui a baixo:")
+nome=input("Insira seu nome: ")
+sexo=input("Insira seu sexo: ")
+idade=int(input("Insira sua idade: "))
+
+print("Faça o cadastro da segunda pessoa aqui a baixo:")
+nome2=input("Insira seu nome: ")
+sexo2=input("Insira seu sexo: ")
+idade2=int(input("Insira sua idade: "))
+
+print("Faça o cadastro da terceira pessoa aqui a baixo:")
+nome3=input("Insira seu nome: ")
+sexo3=input("Insira seu sexo: ")
+idade3=int(input("Insira sua idade: "))
+
+
+print("nome:", nome)
+print("sexo:", sexo)
+print("idade:", idade)
+
+print("nome:", nome2)
+print("idade:", idade2)
+print("sexo:", sexo2)
+
+print("nome", nome3)
+print("sexo:", sexo3)
+print("idade:", idade3)
