055bb14394a3519b41e64103393cffe5070498ea
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-05-07 09:31:09 -0300

    Create tabuada.js

---
+++ tabuada.js
@@ -0,0 +1,37 @@
+let t = 64;
+console.log(t + " x 1 =", t * 1);
+console.log(t + " x 2 =", t * 2);
+console.log(t + " x 3 =", t * 3);
+console.log(t + " x 4 =", t * 4);
+console.log(t + " x 5 =", t * 5);
+console.log(t + " x 6 =", t * 6);
+console.log(t + " x 7 =", t * 7);
+console.log(t + " x 8 =", t * 8);
+console.log(t + " x 9 =", t * 9);
+console.log(t + " x 10 =", t * 10);
+
+t = t + 1;
+
+console.log(t + " x 1 =", t * 1);
+console.log(t + " x 2 =", t * 2);
+console.log(t + " x 3 =", t * 3);
+console.log(t + " x 4 =", t * 4);
+console.log(t + " x 5 =", t * 5);
+console.log(t + " x 6 =", t * 6);
+console.log(t + " x 7 =", t * 7);
+console.log(t + " x 8 =", t * 8);
+console.log(t + " x 9 =", t * 9);
+console.log(t + " x 10 =", t * 10);
+
+t ++;
+
+console.log(t + " x 1 =", t * 1);
+console.log(t + " x 2 =", t * 2);
+console.log(t + " x 3 =", t * 3);
+console.log(t + " x 4 =", t * 4);
+console.log(t + " x 5 =", t * 5);
+console.log(t + " x 6 =", t * 6);
+console.log(t + " x 7 =", t * 7);
+console.log(t + " x 8 =", t * 8);
+console.log(t + " x 9 =", t * 9);
+console.log(t + " x 10 =", t * 10);
