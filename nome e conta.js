c9bf4d5b6737f0b0ec27460f1d6098498104c104
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-05-21 09:43:54 -0300

    Create nome e conta.js

---
+++ nome e conta.js
@@ -0,0 +1,10 @@
+const nome= "Kauany"
+const sobrenome= "Suonski"
+let nomecompleto= ""
+nomecompleto= nome+ " " + sobrenome
+console.log("Seu nome é: ", nomecompleto)
+
+x = Number ("8")
+n = String (8)
+o = "8" + "5"
+console.log("Valor é: ", o)
