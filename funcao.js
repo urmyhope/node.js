00608b65be03b91f3adf70fbae4618ebe353b646
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-05-28 09:44:39 -0300

    Create funcao.js

---
+++ funcao.js
@@ -0,0 +1,20 @@
+    console.log(soma(5,9));
+function soma(V1=0,V2=0){
+    return V1+V2;
+}
+
+
+function nomeidade(nome,idade){
+    console.log(`nome:${nome} - idade:${idade}`);
+}
+nomeidade("Kauany","17");
+nomeidade("17","Kauany");
+console.log(soma(9));
+nomeidade("Suonski");
+
+console.log(multiplicacao(1358,86));
+function multiplicacao(V1=0,V2=0){
+    return V1*V2;
+}
+multiplicacao(soma(3,5),soma(8,7));
+multiplicacao(5);
