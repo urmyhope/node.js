8bcc37c29bac7990b95b25d749b6489abb82fe12
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-06-04 09:38:34 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,42 @@
+
+function adicao(v1, v2){
+    let resutado = 0;
+    return v1 + v2;
+
+}
+
+
+function subtracao(v1, v2){
+    let resutado = 0;
+    return v1 - v2;
+
+}
+
+
+function multiplicacao(v1, v2){
+    let resutado = 0;
+    return v1 * v2;
+
+}
+
+
+function divisao(v1, v2){
+    let resutado = 0;
+    return v1 / v2;
+
+}
+
+function print(mensagem){
+    console.log(mensagem)
+}
+print(adicao(20,33));
+print("Resultado é:" + adicao(8,18));
+
+print(subtracao(20,33));
+print("Resultado é:" + subtracao(8,18));
+
+print(multiplicacao(20,33));
+print("Resultado é:" + multiplicacao(8,18));
+
+print(divisao(20,33));
+print("Resultado é:" + divisao(8,18));
