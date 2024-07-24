029b8054ad7a2da2dc1e958b9a1f4daa75fef863
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-07-24 08:50:47 -0300

    Create objetopessoa.js

---
+++ objetopessoa.js
@@ -0,0 +1,10 @@
+const objetoPessoa={
+    nome:"Kauany",
+    idade:17,
+    cpf:"134.567.290-05"
+}
+console.log(objetoPessoa)
+
+console.log(`nome da pessoa:${objetoPessoa.nome}`)
+
+const lista=["Kauany",17,"134.567.290-05"]
