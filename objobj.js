91492ac8352ef367c0245e0fd9916f4928b80b7c
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-08-06 09:45:09 -0300

    Create objobj.js

---
+++ objobj.js
@@ -0,0 +1,29 @@
+const cliente = {
+    nome: "Hope",
+    idade: 23,
+    email: "hope.s.com",
+    telefone: ["41 93425-9438", "41 91198-8416"]
+}
+
+console.log(cliente)
+
+console.log(cliente.telefone)
+
+console.log(cliente.telefone[1])
+
+
+
+
+cliente.endereco = {
+    rua: 'as andorinhas voltaram',
+    numero: 954,
+    apartamento: true,
+    complemento: "ap 23"
+}
+
+console.log(cliente)
+
+console.log(cliente.endereco)
+
+console.log(cliente.endereco.rua)
+
