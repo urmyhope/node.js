93a93b926f8cc8954601192bb5f6b71978ffabc4
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-08-06 09:44:39 -0300

    Create objcliente.js

---
+++ objcliente.js
@@ -0,0 +1,12 @@
+const cliente = {
+    nome: "Hope",
+    idade: 23,
+    email: "hope.s.com",
+    telefone: ["41 95627-9438", "41 95413-8416"]
+}
+
+console.log(cliente)
+
+console.log(cliente.telefone)
+
+console.log(cliente.telefone[1])
