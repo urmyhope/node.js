a24185f5d3b5499dee73c624a5c350c5fe7cd900
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-08-19 12:18:53 -0300

    Create listafruta.js

---
+++ listafruta.js
@@ -0,0 +1,11 @@
+const listaf = ["melao", "kiwi", "banana", "coco", "manga"]
+
+const maiuscula = []
+
+for(i = 0; i < listaf.length;i++){
+
+maiuscula.push(listaf[i].toUpperCase())
+}
+
+console.log(maiuscula)
+
