8a27af33ceb99ced5e771d0dafbb1a92a14bc139
urmyhope <129955461+urmyhope@users.noreply.github.com>
2024-08-19 08:35:34 -0300

    Create array2.js

---
+++ array2.js
@@ -0,0 +1,16 @@
+const lista=["Monica","Arthur","Pietro"]
+for(i = 0; i <= lista.lengeth;itt){
+    if(lista[i]== "Arthur"){
+        console.log("Existe")
+        console.log("indice:" + i)
+    }
+}
+if(lista.includes("Arthur")){
+        console.log("Existe")
+        i = lista.indexOf("Arthur")
+        console.log("indice:" + i)
+    }
+
+
+
+
